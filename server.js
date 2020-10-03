5b1b88fc42803a5633957b8457ca405061f2d5ce
Vinamra Sareen <vsareen24@gmail.com>
2020-10-03 14:00:17 +0530

    added files

--- server.js
+++ server.js
@@ -1,63 +1,74 @@
-const { default: Axios } = require("axios");
 require("dotenv").config();
 
-const { App } = require("@slack/bolt");
+const { App, ExpressReceiver } = require("@slack/bolt");
+
+// Create a Bolt Receiver
+const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_SECRET });
+
 const slackApp = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  receiver,
   token: process.env.SLACK_BOT_TOKEN,
 });
 
+// Other web requests are methods on receiver.router
+receiver.router.get('/', (req, res) => {
+  // You're working with an express req and res now.
+  res.send('yay!');
+});
+
+
 const token = process.env.token;
 
-// Home Page
-async function appHome(id) {
+// Listen to the app_home_opened Events API event to hear when a user opens your app from the sidebar
+slackApp.event("app_home_opened", async ({ payload, context }) => {
+  const userId = payload.user;
+  
   try {
-    // Call the chat.postMessage method using the built-in WebClient
-    const result = await slackApp.client.views.publish({
-      // You could also use a blocks[] array to send richer content
-      user_id: id,
+    // Call the views.publish method using the built-in WebClient
+    const result = await app.client.views.publish({
+      // The token you used to initialize your app is stored in the `context` object
+      token: context.botToken,
+      user_id: userId,
       view: {
-        type: "home",
-        blocks: [
+        // Home tabs must be enabled in your app configuration page under "App Home"
+        "type": "home",
+        "blocks": [
           {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text:
-                "A simple stack of blocks for the simple sample Block Kit Home tab.",
-            },
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome home, <@" + userId + "> :house:*"
+            }
           },
           {
-            type: "actions",
-            elements: [
-              {
-                type: "button",
-                text: {
-                  type: "plain_text",
-                  text: "Action A",
-                  emoji: true,
-                },
-              },
-              {
-                type: "button",
-                text: {
-                  type: "plain_text",
-                  text: "Action B",
-                  emoji: true,
-                },
-              },
-            ],
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>."
+            }
           },
-        ],
-      },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "context",
+            "elements": [
+              {
+                "type": "mrkdwn",
+                "text": "Psssst this home tab was designed using <https://api.slack.com/tools/block-kit-builder|*Block Kit Builder*>"
+              }
+            ]
+          }
+        ]
+      }
     });
 
-    // Print result, which includes information about the message (like TS)
     console.log(result);
-  } catch (error) {
+  }
+  catch (error) {
     console.error(error);
   }
-}
+});
 
 // Listen to a message containing the substring "hello"
 // app.message requires your app to subscribe to the message.channels event
@@ -88,8 +99,68 @@ async function publishMessage(id, text) {
       // The token you used to initialize your app
       token,
       channel: id,
-      text: text
+      text: text,
+      as_user: true,
       // You could also use a blocks[] array to send richer content
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*"
+          }
+        },
+        {
+          "type": "section",
+          "fields": [
+            {
+              "type": "mrkdwn",
+              "text": "*Type:*\nComputer (laptop)"
+            },
+            {
+              "type": "mrkdwn",
+              "text": "*When:*\nSubmitted Aut 10"
+            },
+            {
+              "type": "mrkdwn",
+              "text": "*Last Update:*\nMar 10, 2015 (3 years, 5 months)"
+            },
+            {
+              "type": "mrkdwn",
+              "text": "*Reason:*\nAll vowel keys aren't working."
+            },
+            {
+              "type": "mrkdwn",
+              "text": "*Specs:*\n\"Cheetah Pro 15\" - Fast, really fast\""
+            }
+          ]
+        },
+        {
+          "type": "actions",
+          "elements": [
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "Approve"
+              },
+              "style": "primary",
+              "value": "click_me_123"
+            },
+            {
+              "type": "button",
+              "text": {
+                "type": "plain_text",
+                "emoji": true,
+                "text": "Deny"
+              },
+              "style": "danger",
+              "value": "click_me_123"
+            }
+          ]
+        }
+      ]
     });
 
     // Print result, which includes information about the message (like TS)
@@ -191,10 +262,10 @@ async function messageEvent() {
   console.log("⚡️ Bolt app is running!");
 
   // Vinamra Id: U01BA9MBDBM
-  // Pompei id:  
+  // Pompei id:  U01BUCASW4S
 
   // Direct or channel message
-  // publishMessage("@Pompei", "Check pompei :tada:");
+  // publishMessage("U01BUCASW4S", "Check pompei :tada:");
 
   // Only visible to you message
   // publishConversation("#general","U01BA9MBDBM", "Shhhh ! Only you can see this !");
@@ -203,7 +274,6 @@ async function messageEvent() {
   // findConversation("general");
 
   // Self message - Impersonate as someone
-  // postAsUser("D01BNN18DB3", "Just a test !");
+  // postAsUser(" ", "Just a test !");
 
-  // appHome("D01BNN18DB3");
 })();
