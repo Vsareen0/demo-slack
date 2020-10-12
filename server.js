755cd600d992637dec5f5c24f16a6e0ca5cc2963
Vinamra Sareen <vsareen24@gmail.com>
2020-10-12 17:28:23 +0530

    check for installation message

--- server.js
+++ server.js
@@ -256,68 +256,6 @@ async function publishMessage(id, text) {
       token: process.env.SLACK_BOT_TOKEN,
       channel: id,
       text: text,
-      // You could also use a blocks[] array to send richer content
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "mrkdwn",
-            text:
-              "You have a new request:\n*<fakeLink.toEmployeeProfile.com|Fred Enriquez - New device request>*",
-          },
-        },
-        {
-          type: "section",
-          fields: [
-            {
-              type: "mrkdwn",
-              text: "*Type:*\nComputer (laptop)",
-            },
-            {
-              type: "mrkdwn",
-              text: "*When:*\nSubmitted Aut 10",
-            },
-            {
-              type: "mrkdwn",
-              text: "*Last Update:*\nMar 10, 2015 (3 years, 5 months)",
-            },
-            {
-              type: "mrkdwn",
-              text: "*Reason:*\nAll vowel keys aren't working.",
-            },
-            {
-              type: "mrkdwn",
-              text: '*Specs:*\n"Cheetah Pro 15" - Fast, really fast"',
-            },
-          ],
-        },
-        {
-          type: "actions",
-          elements: [
-            {
-              type: "button",
-              text: {
-                type: "plain_text",
-                emoji: true,
-                text: "Approve",
-              },
-              action_id: "click_me",
-              style: "primary",
-              value: "click_me_123",
-            },
-            {
-              type: "button",
-              text: {
-                type: "plain_text",
-                emoji: true,
-                text: "Deny",
-              },
-              style: "danger",
-              value: "click_me_123",
-            },
-          ],
-        },
-      ],
     });
 
     // Print result, which includes information about the message (like TS)
